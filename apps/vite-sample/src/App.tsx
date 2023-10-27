c2967d0370a5594dd94fd3730cd85495d2631af0
cahnory <cahnory@gmail.com>
2023-10-27 21:36:48 +0200

    feat(vite-sample-app): setup vite app with react

---
+++ apps/vite-sample/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>
+        <img alt="" aria-hidden height="32" src="/vite.svg" width="32" /> Vite
+        {" + "}
+        <img alt="" aria-hidden height="32" src="/react.svg" width="32" /> React
+      </h1>
+
+      <div>
+        <button
+          onClick={() => {
+            setCount((prevCount) => prevCount + 1);
+          }}
+        >
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
+};
