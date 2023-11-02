d76a3f7e330bc0a67d3a6fb4b782dfe5547e2d7e
cahnory <cahnory@gmail.com>
2023-11-02 22:15:25 +0100

    feat: unit testing using japa in all packages

--- libs/react-sample/japa.unit.ts
+++ libs/react-sample/japa.unit.ts
@@ -1,11 +1,17 @@
 import { expect } from "@japa/expect";
 import { configure, processCLIArgs, run } from "@japa/runner";
+import { setFileScope } from "@vanilla-extract/css/fileScope";
 
 processCLIArgs(process.argv.splice(2));
 configure({
   files: ["src/**/*.spec.ts?(x)"],
   importer: (filePath) => import(filePath.toString()),
-  plugins: [expect()],
+  plugins: [
+    expect(),
+    function vanillaExtractMockPlugin() {
+      setFileScope("mock");
+    },
+  ],
 });
 
 await run();
